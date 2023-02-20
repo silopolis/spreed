e7d47f237eb6dfac62365c1f3f2932976d27d5ab
Daniel Calviño Sánchez <danxuliu@gmail.com>
2023-02-20 03:11:16 +0100

    Use stdeb to build deb packages from Python packages
    Unfortunately stdeb has not been updated yet to use pyproject.toml files
    and still require a setup.py file. Fortunately since setuptools 61.0 the
    configuration from the pyproject.toml file takes precedence and a dummy
    setup.py file loads the configuration from the pyproject.toml file.
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

---
+++ recording/setup.py
@@ -0,0 +1,6 @@
+# Dummy setup.py file to be used by stdeb; setuptools >= 61.0 must be used to
+# get the proper configuration from the pyproject.toml file.
+
+from setuptools import setup
+
+setup()
